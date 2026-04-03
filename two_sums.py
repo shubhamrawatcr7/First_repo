74fe0661b1c53190bf45b347a69aa2827e7fe170
Shubham Rawat <shubhamrawatcr7@gmail.com>
2026-04-03 13:47:25 +0530

    new branch, first commit

--- two_sums.py
+++ two_sums.py
@@ -1,4 +1,5 @@
+name=input("Enter your name: ")
 a=int(input("Enter first number: "))
 b=int(input("Enter second number: "))
 c=a+b
-print(f"Sum of two numbers is: {c}")
+print(f"Hi {name}, sum of two numbers is: {c}")
